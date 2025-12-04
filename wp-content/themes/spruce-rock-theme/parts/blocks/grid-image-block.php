2d21396405210d996e8ae86266035adc15b8e4bb
Roland Flores <juanpeflo2004@hotmail.com>
2025-12-04 18:02:28 -0500

    fixed

--- wp-content/themes/spruce-rock-theme/parts/blocks/grid-image-block.php
+++ wp-content/themes/spruce-rock-theme/parts/blocks/grid-image-block.php
@@ -8,28 +8,45 @@ $image_id = get_field('grid_image_main_image');
 $image_url = '';
 if( $image_id ) {
     list( $image_url ) = wp_get_attachment_image_src( $image_id, 'huge' );
+}else{
+    $grid_image_title = preg_replace(
+        '/(<br\s*\/?>)([^<]+)$/i',
+        '$1<span class="last-text">$2</span>',
+        $grid_image_title
+    );
 }
 ?>
 <section class="grid-image">
     <div class="container">
-        <div class="grid-image__content">
+        <?php if($grid_image_eyebrow_text && !$image_url): ?>
+        <div class="grid-image__right-eyebrow bottom-40 t-caption t-trim">
+            <?=$grid_image_eyebrow_text?>
+        </div>
+        <?php endif; ?>
+        <div class="grid-image__content <?= (!$image_url)?'grid-image__content--top':'' ?>">
             <?php if($image_url): ?>
-            <div class="grid-image__left <?= ($grid_image_position == 'right')?'grid-image__left--change':'' ?>">
+            <div class="grid-image__left <?= ($grid_image_position == 'right')?'':'grid-image__left--change' ?>">
                 <img class="grid-image__left-image" src="<?= esc_url($image_url); ?>">
+            </div>
+            <?php else: ?>
+            <div class="grid-image__right-text <?= ($grid_image_position == 'right')?'':'grid-image__left--change' ?> flow t-18 t-trim">
+                <?=$grid_image_text?>
             </div>
             <?php endif; ?>
             <div class="grid-image__right flow">
-                <?php if($grid_image_eyebrow_text): ?>
+                <?php if($grid_image_eyebrow_text && $image_url): ?>
                 <div class="grid-image__right-eyebrow t-caption t-trim">
                     <?=$grid_image_eyebrow_text?>
                 </div>
                 <?php endif; ?>
-                <h2 class="grid-image__right-title <?= ($grid_image_eyebrow_text)?'t-96':'t-96-56' ?> t-trim t-uppercase">
+                <h2 class="grid-image__right-title <?= ($grid_image_eyebrow_text && $image_url)?'t-96':'t-96-56' ?> t-trim t-uppercase">
                     <?=$grid_image_title?>
                 </h2>
+                <?php if($image_url): ?>
                 <div class="grid-image__right-text flow t-18 t-trim">
                     <?=$grid_image_text?>
                 </div>
+                <?php endif; ?>
                 <?php if($template_buttons){
                     the_buttons($template_buttons); 
                 }?> 
